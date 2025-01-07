e8c0c16f2e9af4107d2cd5a2c914af28ca2bbf15
Vlad <ricean.vlad7@gmail.com>
2025-01-07 16:00:55 +0200

    Network Transform to sync positions

--- Assets/Scripts/GameVisualManager.cs
+++ Assets/Scripts/GameVisualManager.cs
@@ -1,7 +1,8 @@
 using System;
+using Unity.Netcode;
 using UnityEngine;
 
-public class GameVisualManager : MonoBehaviour
+public class GameVisualManager : NetworkBehaviour
 {
 
     private const float GRID_SIZE = 3.1f;
@@ -16,9 +17,18 @@ public class GameVisualManager : MonoBehaviour
     
     private void OnClickedOnGridPosition(object sender, GameManager.ClickedOnGridPositionEventArgs e)
     {
-        Instantiate(crossPrefab, GetGridWorldPosition(e.x, e.y), Quaternion.identity);
+        SpawnObjectRpc(e.x, e.y);
     }
-    
+
+    [Rpc(SendTo.Server)]
+    private void SpawnObjectRpc(int x, int y)
+    {
+        Debug.Log("SpawnObject: " + x + ", " + y);
+        Transform spawnedCrossTransform = Instantiate(crossPrefab);
+        spawnedCrossTransform.GetComponent<NetworkObject>().Spawn(destroyWithScene:true);
+        spawnedCrossTransform.position = GetGridWorldPosition(x, y);
+    }
+
     private Vector2 GetGridWorldPosition(int x, int y)
     {
         return new Vector2(-GRID_SIZE + x * GRID_SIZE, -GRID_SIZE + y * GRID_SIZE);
