ef12fac6572fc153c0ef86bd054c562ebd939c53
Vlad <ricean.vlad7@gmail.com>
2025-01-07 13:58:42 +0200

    Add GridPosition script

---
+++ Assets/Scripts/GridPosition.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+public class GridPosition : MonoBehaviour
+{
+    [SerializeField] private int x;
+    [SerializeField] private int y;
+    
+    private void OnMouseDown()
+    {
+        Debug.Log("GridPosition: " + transform.position);
+    }
+}
