d107b21e103aecb839893d34e5dd19d5a4ec21ed
Vlad <ricean.vlad7@gmail.com>
2025-01-07 17:13:22 +0200

    Use NetworkVariable

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,25 +5,31 @@ using UnityEngine;
 public class GameManager : NetworkBehaviour
 {
     public static GameManager Instance { get; private set; }
-    
+
     public event EventHandler<ClickedOnGridPositionEventArgs> OnClickedOnGridPosition;
+
     public class ClickedOnGridPositionEventArgs : EventArgs
     {
         public int x;
         public int y;
         public PlayerType playerType;
     }
-    
+
+    public event EventHandler OnGameStarted;
+    public event EventHandler OnCurrentPlayablePlayerTypeChanged;
+
     public enum PlayerType
     {
-        None, 
+        None,
         Cross,
         Circle
     }
-    
+
     private PlayerType localPlayerType;
     public PlayerType LocalPlayerType => localPlayerType;
     
+    private NetworkVariable<PlayerType> currentPlayablePlayerType = new NetworkVariable<PlayerType>(PlayerType.None);
+
     private void Awake()
     {
         if (Instance == null)
@@ -36,12 +42,11 @@ public class GameManager : NetworkBehaviour
         }
     }
 
-    private PlayerType currentPlayablePlayerType;
-    
+
     public override void OnNetworkSpawn()
     {
         Debug.Log("OnNetworkSpawn + NetworkManager.Singleton.LocalClientId: " + NetworkManager.Singleton.LocalClientId);
-        
+
         if (NetworkManager.Singleton.LocalClientId == 0)
         {
             localPlayerType = PlayerType.Cross;
@@ -53,22 +58,48 @@ public class GameManager : NetworkBehaviour
 
         if (IsServer)
         {
-            currentPlayablePlayerType = PlayerType.Cross;
+            NetworkManager.Singleton.OnClientConnectedCallback += OnClientConnected;
+        }
+        
+        currentPlayablePlayerType.OnValueChanged += (previous, current) =>
+        {
+            OnCurrentPlayablePlayerTypeChanged?.Invoke(this, EventArgs.Empty);
+        };
+    }
+
+    private void OnClientConnected(ulong clientId)
+    {
+        if (NetworkManager.Singleton.ConnectedClientsList.Count == 2)
+        {
+            currentPlayablePlayerType.Value = PlayerType.Cross;
+            TriggerOnGameStartedRpc();
         }
     }
-    
+
+    [Rpc(SendTo.ClientsAndHost)]
+    private void TriggerOnGameStartedRpc()
+    {
+        OnGameStarted?.Invoke(this, EventArgs.Empty);
+    }
+
     [Rpc(SendTo.Server)]
     public void ClickedOnGridPositionRpc(int x, int y, PlayerType playerType)
     {
-        if (playerType != currentPlayablePlayerType)
+        if (playerType != currentPlayablePlayerType.Value)
         {
             return;
         }
-        
+
         Debug.Log("GameManager: " + x + ", " + y);
-        OnClickedOnGridPosition?.Invoke(this, new ClickedOnGridPositionEventArgs { x = x, y = y, playerType = playerType});
-        
-        currentPlayablePlayerType = currentPlayablePlayerType == PlayerType.Cross ? PlayerType.Circle : PlayerType.Cross;
+        OnClickedOnGridPosition?.Invoke(this,
+            new ClickedOnGridPositionEventArgs { x = x, y = y, playerType = playerType });
+
+        currentPlayablePlayerType.Value =
+            currentPlayablePlayerType.Value == PlayerType.Cross ? PlayerType.Circle : PlayerType.Cross;
     }
-    
+
+    public PlayerType GetCurrentPlayablePlayerType()
+    {
+        return currentPlayablePlayerType.Value;
+    }
 }
---
+++ Assets/Scripts/PlayerUI.cs
@@ -0,0 +1,57 @@
+using System;
+using UnityEngine;
+
+public class PlayerUI : MonoBehaviour
+{
+    [SerializeField] private GameObject crossArrowGameObject;
+    [SerializeField] private GameObject circleArrowGameObject;
+    [SerializeField] private GameObject crossYouTextGameObject;
+    [SerializeField] private GameObject circleYouTextGameObject;
+
+    private void Awake()
+    {
+        crossArrowGameObject.SetActive(false);
+        circleArrowGameObject.SetActive(false);
+        crossYouTextGameObject.SetActive(false);
+        circleYouTextGameObject.SetActive(false);
+    }
+    
+    private void Start()
+    {
+        GameManager.Instance.OnGameStarted += OnGameStarted;
+        GameManager.Instance.OnCurrentPlayablePlayerTypeChanged += OnCurrentPlayablePlayerTypeChanged;
+    }
+    
+    private void OnCurrentPlayablePlayerTypeChanged(object sender, EventArgs e)
+    {
+        UpdateCurrentArrow();
+    }
+    
+    private void OnGameStarted(object sender, EventArgs e)
+    {
+        if (GameManager.Instance.LocalPlayerType == GameManager.PlayerType.Cross)
+        {
+            crossYouTextGameObject.SetActive(true);
+        }
+        else
+        {
+            circleYouTextGameObject.SetActive(true);
+        }
+        
+        UpdateCurrentArrow();
+    }
+    
+    private void UpdateCurrentArrow()
+    {
+        if(GameManager.Instance.GetCurrentPlayablePlayerType() == GameManager.PlayerType.Cross)
+        {
+            crossArrowGameObject.SetActive(true);
+            circleArrowGameObject.SetActive(false);
+        }
+        else
+        {
+            crossArrowGameObject.SetActive(false);
+            circleArrowGameObject.SetActive(true);
+        }
+    }
+}
