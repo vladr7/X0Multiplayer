004c22bc88ebdfb25842481952fe0cfd902765dc
Vlad <ricean.vlad7@gmail.com>
2025-01-08 09:34:40 +0200

    Win lose sfx

--- Assets/SoundManager.cs
+++ Assets/SoundManager.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
     [SerializeField] private Transform placeSfxPrefab;
+    [SerializeField] private Transform winSfxPrefab;
+    [SerializeField] private Transform loseSfxPrefab;
 
     private void Start()
     {
         GameManager.Instance.OnPlacedObject += OnPlacedObject;
+        GameManager.Instance.OnGameWin += OnGameWin;
     }
-    
+
+    private void OnGameWin(object sender, GameManager.OnGameWinEventArgs e)
+    {
+        Transform sfx =
+            Instantiate(e.winPlayerType == GameManager.Instance.LocalPlayerType ? winSfxPrefab : loseSfxPrefab,
+                transform.position, Quaternion.identity);
+        Destroy(sfx.gameObject, 5f);
+    }
+
     private void OnPlacedObject(object sender, EventArgs e)
     {
         Transform sfx = Instantiate(placeSfxPrefab, transform.position, Quaternion.identity);
